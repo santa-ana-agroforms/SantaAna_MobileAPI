498899b12c9a41653a89873225cd933feeb4d099
Danval-003 <danarvare2003@gmail.com>
2025-10-22 21:50:38 -0600

    fix: data analisys

--- src/forms/entities/pagina.entity.ts
+++ src/forms/entities/pagina.entity.ts
@@ -1,15 +1,7 @@
-import {
-  Check,
-  Column,
-  Entity,
-  ManyToOne,
-  OneToMany,
-  PrimaryColumn,
-  JoinColumn,
-} from 'typeorm';
-import { Formulario } from './formulario.entity';
-import { FormularioVersion } from './formulario-version.entity';
+// src/entities/pagina.entity.ts
+import { Check, Column, Entity, OneToMany, PrimaryColumn } from 'typeorm';
 import { PaginaIndexVersion } from './pagina-index-version.entity';
+import { PaginaVersion } from './pagina-version.entity';
 
 @Entity({ name: 'formularios_pagina' })
 @Check('secuencia >= 0')
@@ -26,25 +18,11 @@ export class Pagina {
   @Column({ type: 'text' })
   descripcion!: string;
 
-  @Column({ type: 'uuid', name: 'formulario_id' })
-  formularioId!: string;
-
-  @Column({ type: 'uuid', name: 'index_version_id' })
-  indexVersionId!: string;
-
-  @ManyToOne(() => Formulario, (f) => f.paginas, { onDelete: 'CASCADE' })
-  @JoinColumn({ name: 'formulario_id', referencedColumnName: 'id' })
-  formulario!: Formulario;
-
-  @ManyToOne(() => FormularioVersion, (v) => v.paginasLink, {
-    onDelete: 'CASCADE',
-  })
-  @JoinColumn({
-    name: 'index_version_id',
-    referencedColumnName: 'idIndexVersion',
-  })
-  indexVersion!: FormularioVersion;
-
+  // Enlace a versiones de formulario vía tabla puente
   @OneToMany(() => PaginaIndexVersion, (piv) => piv.pagina)
   indexVersionLink!: PaginaIndexVersion[];
+
+  // Versiones propias de la página (formularios_pagina_version)
+  @OneToMany(() => PaginaVersion, (pv) => pv.pagina)
+  versiones!: PaginaVersion[];
 }
